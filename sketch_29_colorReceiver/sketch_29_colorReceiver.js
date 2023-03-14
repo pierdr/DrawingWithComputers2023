009c1ca4073be3ffb8cc92ebf980812bbec477d3
Pierluigi Dalla Rosa <pierdr@yahoo.it>
2023-03-13 21:40:30 -0700

    color receiver

---
+++ sketch_29_colorReceiver/sketch_29_colorReceiver.js
@@ -0,0 +1,46 @@
+let socket;
+let receivedColor;
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+  receivedColor = color(0,0,0);
+  // Connect to web socket server on port 8009
+  socket = new WebSocket('ws://localhost:8009');
+
+  // When a message is received from the server
+  socket.onmessage = function(event) {
+    console.log(event);
+    let data = JSON.parse(event.data);
+    receivedColor = color(data.col.r, data.col.g, data.col.b);
+    fill(receivedColor);
+    rect(0, 0, width, height);
+  };
+}
+
+function draw() {
+  // No need to draw anything here
+}
+
+function keyPressed() {
+  // When the spacebar is pressed
+  if (keyCode === 32) {
+    // Change the color to a variant of the original color
+    let newColor = color(
+      255-red(receivedColor),
+      255-green(receivedColor),
+      255-blue(receivedColor)
+    );
+    fill(newColor);
+    rect(0, 0, width, height);
+
+    // Send the new color back to the server
+    let message = {
+      col: {
+        r: red(newColor),
+        g: green(newColor),
+        b: blue(newColor)
+      }
+    };
+    socket.send(JSON.stringify(message));
+  }
+}
